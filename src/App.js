91523521ad243e8f4ad0e4afdd6af36f032a4a8a
no2ehi <graphist77@gmail.com>
2023-01-26 19:11:15 +0330

    add initial todo items data

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Todos from './components/Todos';
+import dayjs from 'dayjs';
 
 function App() {
 
-  const [listTasks, setListTasks] = useState([]);
+    const todoData = [
+        {
+            _id: 1,
+            title: 'Read boyd language book',
+            status: false,
+            deadline: dayjs('2020-09-18T17:11:54'),
+        },
+        {
+            _id: 2,
+            title: 'Do My Home Work',
+            status: true,
+            deadline: dayjs('2012-02-18T10:16:04'),
+        },
+        {
+            _id: 3,
+            title: 'create mini project react',
+            status: false,
+            deadline: dayjs('2018-08-18T21:11:54'),
+        },
+    ]
+
+  const [listTasks, setListTasks] = useState(todoData);
   const [showListTasks, setShowListTasks] = useState([]);
   const [modeSort, setModeSort] = useState('All');
 
