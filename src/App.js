17ff73608deffb31d31ed2e953becea113dbd943
no2ehi <graphist77@gmail.com>
2023-01-26 11:58:15 +0330

    add useEffect & HandleSort function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Todos from './components/Todos';
@@ -6,6 +6,8 @@ import Todos from './components/Todos';
 function App() {
 
   const [listTasks, setListTasks] = useState([]);
+  const [showListTasks, setShowListTasks] = useState([]);
+  const [modeSort, setModeSort] = useState('All');
 
   const handleSubmit = (task) => {
     setListTasks([
@@ -51,15 +53,38 @@ function App() {
     )
   }
 
+  const handleSortList = (mode) => {
+    setModeSort(mode);
+  }
+
+  useEffect(() => {
+    if( modeSort === 'All') {
+        setShowListTasks(
+            listTasks
+        )
+    } else if ( modeSort === 'Incomplete') {
+        let sortedListTasks = listTasks.filter( t =>  !t.status  );
+        setShowListTasks(
+            sortedListTasks
+        )
+    } else {
+        let sortedListTasks = listTasks.filter( t => t.status  );
+        setShowListTasks(
+            sortedListTasks
+        )
+    }
+  },[listTasks, modeSort])
+
 
   return (
     <div className="flex flex-col items-center w-full h-full bg-white my-10 gap-6">
             <h1 className="text-4xl font-bold uppercase text-gray-600">ToDo List</h1>
             <Header 
                 handleSubmit={handleSubmit}
+                sortHandler={handleSortList}
              />
             <Todos 
-                tasks={listTasks}
+                tasks={showListTasks}
                 checkHandler={handleCheck}
                 deleteHandler={handleDelete}
                 editeHandler={handleEdit}  />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 import DialogTodoItem from './DialogTodoItem';
 
-const Header = ({ handleSubmit }) => {
+const Header = ({ handleSubmit, sortHandler }) => {
     const [open, setOpen] = useState(false);
 
     return(
         <div className="flex justify-between w-2/4">
             <button onClick={() => setOpen(true)}
              className="bg-indigo-500 hover:bg-indigo-600 text-white font-medium rounded-md px-5 py-2">Add Task</button>
-            <select 
+            <select onChange={(e) => sortHandler(e.target.value)}
              className="bg-gray-300 rounded-lg font-medium text-gray-600 px-4 py-2" >
                 <option value="All">All</option>
                 <option value="Incomplete">Incomplete</option>
